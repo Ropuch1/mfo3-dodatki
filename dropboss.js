f5433f641f3f9bd657d8ecfbc9de73f18862e0f7
Ropuch1 <marshadov2020@gmail.com>
2026-05-11 20:37:09 +0200

    Create dropboss.js

---
+++ dropboss.js
@@ -0,0 +1,21 @@
+(function() {
+    let lastBattleId = "";
+    setInterval(() => {
+        const dialog = document.querySelector('.BattleResultsDialog');
+        if (dialog && dialog.offsetParent !== null) {
+            const replayInput = dialog.querySelector('.replay-link input');
+            const currentId = replayInput ? replayInput.value : "";
+
+            if (currentId && currentId !== lastBattleId) {
+                lastBattleId = currentId;
+                // Szuka potwora (M:) lub pierwszego przegranego
+                const enemy = dialog.querySelector('.fighter[real_id^="M:"]') || 
+                              dialog.querySelector('.loosers .fighter');
+                if (enemy) {
+                    setTimeout(() => enemy.click(), 150);
+                }
+            }
+        }
+    }, 300);
+    console.log("Skrypt na auto-drop aktywny.");
+})();
