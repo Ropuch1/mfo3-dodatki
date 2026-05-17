bcff8dbaeb54e49c0c91441f3a62e4ff47b7e3f9
Ropuch1 <marshadov2020@gmail.com>
2026-05-17 20:48:41 +0200

    Update kill.js

--- kill.js
+++ kill.js
@@ -15,6 +15,7 @@
         }
 
         if (event.code === TRIGGER_CODE) {
+            event.preventDefault();
             if (typeof window.clickNearestMonsterDOM === 'function') {
                 window.clickNearestMonsterDOM();
             }
@@ -22,71 +23,93 @@
     }, true);
 
     const injectedCode = function() {
+        let retryInterval = null;
+
         window.clickNearestMonsterDOM = function() {
-            if (typeof MapEngine === 'undefined' || !MapEngine.instance) {
-                return;
+            if (retryInterval) return;
+
+            function tryAttack() {
+                if (typeof MapEngine === 'undefined' || !MapEngine.instance || !document.id('_player_position')) {
+                    return false;
+                }
+
+                const engine = MapEngine.instance;
+                const posEl = document.id('_player_position');
+                const [playerX, playerY] = posEl.get('text').split(',').map(num => parseInt(num.trim()));
+
+                const monsterElements = document.getElements('div[id^="monster_"][id$="_dom"]');
+                let closestMonsterDOM = null;
+                let minDistance = Infinity;
+
+                monsterElements.each(function(el) {
+                    const overlay = el.getNext('.overlay');
+                    if (overlay && overlay.getStyle('visibility') !== 'hidden' && overlay.getStyle('opacity') !== '0') {
+                        const rawId = el.get('id');
+                        const cleanId = rawId.replace('_dom', '');
+
+                        let monsterX, monsterY;
+                        const engineNPC = (engine.npcs && engine.npcs[cleanId]) || 
+                                          (engine.map && engine.map.events && engine.map.events[cleanId]);
+
+                        if (engineNPC && engineNPC.x !== undefined && engineNPC.y !== undefined) {
+                            monsterX = engineNPC.x;
+                            monsterY = engineNPC.y;
+                        } else {
+                            const left = parseInt(el.getStyle('left'));
+                            const top = parseInt(el.getStyle('top'));
+                            const height = parseInt(el.getStyle('height')) || 80;
+                            if (!isNaN(left) && !isNaN(top)) {
+                                monsterX = Math.round(left / 32);
+                                monsterY = Math.round((top + height - 16) / 32);
+                            }
+                        }
+
+                        if (monsterX !== undefined && monsterY !== undefined) {
+                            const distance = Math.abs(playerX - monsterX) + Math.abs(playerY - monsterY);
+                            if (distance < minDistance) {
+                                minDistance = distance;
+                                closestMonsterDOM = el;
+                            }
+                        }
+                    }
+                });
+
+                if (closestMonsterDOM) {
+                    const fakeEvent = {
+                        stop: function() {},
+                        target: closestMonsterDOM,
+                        type: 'click'
+                    };
+
+                    if (typeof closestMonsterDOM.fireEvent === 'function') {
+                        closestMonsterDOM.fireEvent('click', fakeEvent);
+                    }
+
+                    const overlayEl = closestMonsterDOM.getNext('.overlay');
+                    if (overlayEl && typeof overlayEl.fireEvent === 'function') {
+                        fakeEvent.target = overlayEl;
+                        overlayEl.fireEvent('click', fakeEvent);
+                    }
+                    return true;
+                }
+                return false;
             }
 
-            const engine = MapEngine.instance;
-            const posEl = document.id('_player_position');
-            if (!posEl) return;
-            const [playerX, playerY] = posEl.get('text').split(',').map(num => parseInt(num.trim()));
-
-            const monsterElements = document.getElements('div[id^="monster_"][id$="_dom"]');
-            
-            let closestMonsterDOM = null;
-            let minDistance = Infinity;
-
-            monsterElements.each(function(el) {
-                const overlay = el.getNext('.overlay');
-                if (overlay && overlay.getStyle('visibility') !== 'hidden' && overlay.getStyle('opacity') !== '0') {
-                    
-                    const rawId = el.get('id');
-                    const cleanId = rawId.replace('_dom', '');
-
-                    let monsterX, monsterY;
-                    const engineNPC = (engine.npcs && engine.npcs[cleanId]) || 
-                                      (engine.map && engine.map.events && engine.map.events[cleanId]);
-
-                    if (engineNPC && engineNPC.x !== undefined && engineNPC.y !== undefined) {
-                        monsterX = engineNPC.x;
-                        monsterY = engineNPC.y;
-                    } else {
-                        const left = parseInt(el.getStyle('left'));
-                        const top = parseInt(el.getStyle('top'));
-                        const height = parseInt(el.getStyle('height')) || 80;
-                        if (!isNaN(left) && !isNaN(top)) {
-                            monsterX = Math.round(left / 32);
-                            monsterY = Math.round((top + height - 16) / 32);
-                        }
+            const success = tryAttack();
+            if (!success) {
+                retryInterval = setInterval(function() {
+                    if (tryAttack()) {
+                        clearInterval(retryInterval);
+                        retryInterval = null;
                     }
+                }, 50);
 
-                    if (monsterX !== undefined && monsterY !== undefined) {
-                        const distance = Math.abs(playerX - monsterX) + Math.abs(playerY - monsterY);
-                        if (distance < minDistance) {
-                            minDistance = distance;
-                            closestMonsterDOM = el;
-                        }
+                setTimeout(function() {
+                    if (retryInterval) {
+                        clearInterval(retryInterval);
+                        retryInterval = null;
                     }
-                }
-            });
-
-            if (closestMonsterDOM) {
-                const fakeEvent = {
-                    stop: function() {},
-                    target: closestMonsterDOM,
-                    type: 'click'
-                };
-
-                if (typeof closestMonsterDOM.fireEvent === 'function') {
-                    closestMonsterDOM.fireEvent('click', fakeEvent);
-                }
-
-                const overlayEl = closestMonsterDOM.getNext('.overlay');
-                if (overlayEl && typeof overlayEl.fireEvent === 'function') {
-                    fakeEvent.target = overlayEl;
-                    overlayEl.fireEvent('click', fakeEvent);
-                }
+                }, 4000);
             }
         };
     };
