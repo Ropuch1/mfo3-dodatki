a44ff2f7fa0c5e013fe3520a34d7386dcdae4ce9
Ropuch1 <marshadov2020@gmail.com>
2026-06-06 19:08:21 +0200

    Create muzyka.js

---
+++ muzyka.js
@@ -0,0 +1,36 @@
+(function() {
+    'use strict';
+    
+    // Pobranie ustawień zapisanych przez panel
+    const getSetting = (id) => localStorage.getItem(`mfo3_val_muzyka_${id}`);
+    
+    let bgMusic = new Audio(getSetting('m_link') || "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3");
+    bgMusic.loop = true;
+    let isPlaying = false;
+
+    function monitorMusic() {
+        const titleEl = document.getElementById('MapBox_title');
+        const targetMap = getSetting('m_mapa') || "Eternia";
+        const currentMap = titleEl ? titleEl.innerText.trim() : "";
+        const currentLink = getSetting('m_link');
+
+        // Aktualizacja źródła, jeśli link się zmienił w panelu
+        if (bgMusic.src !== currentLink && currentLink) {
+            bgMusic.src = currentLink;
+        }
+
+        if (currentMap === targetMap) {
+            if (!isPlaying) {
+                bgMusic.play().catch(() => {});
+                isPlaying = true;
+            }
+        } else {
+            if (isPlaying) {
+                bgMusic.pause();
+                isPlaying = false;
+            }
+        }
+    }
+
+    setInterval(monitorMusic, 1000);
+})();
