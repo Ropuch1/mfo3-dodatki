67d511c5c4e83d28ad42c2c95b803338cb48c0e7
Ropuch1 <marshadov2020@gmail.com>
2026-06-06 19:35:58 +0200

    Create przegrana.js

---
+++ przegrana.js
@@ -0,0 +1,25 @@
+(function() {
+    'use strict';
+
+    // Pobranie ustawień z localStorage (panel sam będzie je zapisywał)
+    const getSetting = (id) => localStorage.getItem(`mfo3_val_przegrana_${id}`);
+    
+    // Funkcja odtwarzająca dźwięk
+    function playDefeatSound() {
+        const soundUrl = getSetting('s_link') || "";
+        if (!soundUrl) return;
+        
+        const audio = new Audio(soundUrl);
+        audio.volume = parseFloat(localStorage.getItem('mfo3_defeat_volume')) || 0.5;
+        audio.play().catch(e => console.log("Przegrana: Kliknij w grę, aby odblokować dźwięk"));
+    }
+
+    // Monitorowanie walki
+    setInterval(() => {
+        const resultDialog = document.querySelector('.BattleResultsDialog.looser');
+        if (resultDialog && resultDialog.getAttribute('data-defeat-notified') !== 'true') {
+            resultDialog.setAttribute('data-defeat-notified', 'true');
+            playDefeatSound();
+        }
+    }, 500);
+})();
